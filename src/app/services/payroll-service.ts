63e3e07b59a4fd5ecdd8a46088ee2aa79505b83f
storrealbaconf <storrealba@ext.confuturo.cl>
2026-02-17 15:17:45 -0300

    back a backend

--- src/app/services/payroll-service.ts
+++ src/app/services/payroll-service.ts
@@ -67,7 +67,6 @@ export class PayrollService {
     return this._http.post<DataWithStatus<Settlement[]>>(`${this.apiUrl}/get-settlements`, filters);
   }
 
-  // Get history
   getHistory(): Observable<Payroll[]> {
     const mockData: Payroll[] = [
       {
